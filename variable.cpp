02750fb1dfafe57030160364a69ca7115cf2c090
Siddharth Datthathreya <marshtomp.1999@hotmail.com>
2020-11-29 10:53:26 +0530

    Variable Templatewith area of circl

--- variable.cpp
+++ variable.cpp
@@ -5,7 +5,13 @@ using namespace std;
 template<typename T>
 T pi = T(3.1415926535897932385L);
 
+template<typename T>
+T area_of_circle(const T &r){
+    return r * r * pi<T>;
+}
+
 int main(){
     cout.precision(20);
-    cout<< pi<double> <<endl;
+    cout<< pi<long double> <<endl;
+    cout<< area_of_circle<long double>(3)<<endl;
 }
