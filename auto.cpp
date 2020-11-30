c39d0a0dcfbb1202b4313b7ddb39996a44d79abb
Siddharth Datthathreya <marshtomp.1999@hotmail.com>
2020-11-30 08:45:27 +0530

    Auto Variable Function Template

---
+++ auto.cpp
@@ -0,0 +1,38 @@
+// auto.cpp by Bill Weinman <http://bw.org/>
+// updated 2018-11-02
+#include <iostream>
+#include <typeinfo>
+#include <string>
+using namespace std;
+
+template <typename lT, typename rT>
+auto tf(const lT & lhs, const rT & rhs){
+    return rhs +lhs;
+}
+
+int main() {
+    int i = 47;
+    const char * cstr = "this is a c-string";
+    const string sclass = string("this is a string class string");
+    
+    auto x = "this is a c-string";
+    decltype(x) y;
+    
+    cout << "type of i is " << typeid(i).name() << endl;
+    cout << "type of cstr is " << typeid(cstr).name() << endl;
+    cout << "type of sclass is " << typeid(sclass).name() << endl;
+    cout << "type of x is " << typeid(x).name() << endl;
+    cout << "type of y is " << typeid(y).name() << endl;
+    //string class iterator
+    for(auto it=sclass.begin(); it!=sclass.end();it++){
+        cout<< *it<<" ";
+    }
+
+    auto z = tf<string, const char *>(sclass, cstr);
+    cout<<"z is "<<z<<endl;
+    cout<<"type of z is "<<typeid(z).name()<<endl;
+    cout<<endl;
+    //for(auto c: sclass){ cout<< c <<" ";} cout<<endl;
+
+    return 0;
+}
