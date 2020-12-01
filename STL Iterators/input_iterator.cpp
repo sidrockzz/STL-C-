e92f4d578bd20a734898be70401780fc120b4bdb
Siddharth Datthathreya <marshtomp.1999@hotmail.com>
2020-12-01 16:24:42 +0530

    Input iterator

---
+++ STL Iterators/input_iterator.cpp
@@ -0,0 +1,34 @@
+// input-iterator.cpp by Bill Weinman <http://bw.org/>
+// 2018-09-18 for CppSTL
+#include<bits/stdc++.h>
+using namespace std;
+
+void message(const char * m) { cout << m << endl; }
+
+int main() {
+    double d1 = 0, d2 = 0;
+    
+    cout << "Two numeric values: " << flush;
+    cin.clear();
+    istream_iterator<double> eos;         // default constructor is end-of-stream
+    istream_iterator<double> iit(cin);    // stdin iterator
+
+    if(iit == eos) {
+        message("no values");
+        return 0;
+    } else {
+        d1 = *iit++;
+    }
+    
+    if(iit == eos) {
+        message("no second value");
+        return 0;
+    } else {
+        d2 = *iit;
+    }
+
+    cin.clear();
+    cout << d1 << " * " << d2 << " = " << d1 * d2 << endl;
+
+    return 0;
+}
