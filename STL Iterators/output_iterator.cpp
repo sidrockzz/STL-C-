368c8d77ae70e9fcd67fe134455109fff86d2bd9
Siddharth Datthathreya <marshtomp.1999@hotmail.com>
2020-12-01 16:30:09 +0530

    Output and Bidirectional Iterator

---
+++ STL Iterators/output_iterator.cpp
@@ -0,0 +1,16 @@
+// output-iterator.cpp by Bill Weinman <http://bw.org/>
+// 2018-09-18 for CppSTL
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ostream_iterator<int> output(cout, " ");
+    
+    for(int i : { 3, 197, 42 }) {
+        *output++ = i;
+    }
+    cout << endl;
+
+    return 0;
+}
